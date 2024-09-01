4c3b466ebfce416413eaa086aa5791983c88f9de
yahyahetari <132639815+yahyahetari@users.noreply.github.com>
2024-09-01 06:44:38 +0300

    hgk

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,12 @@ import ProductsList from "@/components/ProductsList";
 import { connectToDB } from "@/lib/mongoose";
 import { Product } from "@/models/Products";
 import { useState, useEffect } from "react";
-import Loader from "@/components/Loader"; // تأكد من استيراد مكون Loader
+import Loader from "@/components/Loader";
 
 export default function Home({productsList}) {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // محاكاة تحميل البيانات
         setTimeout(() => {
             setLoading(false);
         }, 1000);
@@ -24,7 +23,8 @@ export default function Home({productsList}) {
 
     return (
         <div>
-            <img src="/banner.png"/>
+            <img src="/banner.png" className="hidden md:block w-full" alt="Banner"/>
+            <img src="/banner1.png" className="block md:hidden w-full" alt="Banner 1"/>
             <div>
                 <h1 className="text-3xl ml-20 m-2 font-semibold">New Arrivals</h1>
                 <ProductsList products={productsList} />
@@ -33,12 +33,12 @@ export default function Home({productsList}) {
     );
 }
 
-export async  function getServerSideProps() {
+export async function getServerSideProps() {
   await connectToDB()
   const productsList = await Product.find({},null,{sort : {'_id':-1} })
   return {
     props: {
       productsList: JSON.parse(JSON.stringify(productsList)),
-      }
-      }
+    }
+  }
 }
